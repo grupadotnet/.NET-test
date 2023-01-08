081f2a0593dd230c0e3ba5677860fa73ff98ad03
Dawid <dawid123wilgucki@wp.pl>
2023-01-08 17:11:50 +0100

    first commit

---
+++ Repository/CharactersRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ACM.BL
+{
+    public class CustomerRepository
+    {
+        public Customer Find(List<Customer> customerList, int customerId)
+        {
+            Customer foundCustomer = null;
+
+
+            foundCustomer = customerList.FirstOrDefault(c =>
+                                c.CustomerId == customerId);
+
+
+
+            return foundCustomer;
+
+        }
+
+        public List<Customer> Retrieve()
+        {
+            List<Customer> custList = new List<Customer>
+                    {new Customer()
+                          { CustomerId = 1,
+                            FirstName="Luke",
+                            LastName = "Skywalker",
+                            EmailAddress = "ls@hob.me",
+                            CustomerTypeId=1},
+                    new Customer()
+                          { CustomerId = 2,
+                            FirstName="Anakin",
+                            LastName = "Skywalker",
+                            EmailAddress = "as@hob.me",
+                            CustomerTypeId=null},
+                    new Customer()
+                          { CustomerId = 3,
+                            FirstName="Darth",
+                            LastName = "Vader",
+                            EmailAddress = "dv@hob.me",
+                            CustomerTypeId=1},
+                    new Customer()
+                          { CustomerId = 4,
+                            FirstName="Han",
+                            LastName = "Solo",
+                            EmailAddress = "hs@hob.me",
+                            CustomerTypeId=2}};
+            return custList;
+        }
+        public IEnumerable<Customer> SortByName(List<Customer> customerList)
+        {
+            return customerList.OrderBy(c => c.LastName)
+                            .ThenBy(c => c.FirstName);
+        }
+        public IEnumerable<Customer> SortByNameInReverse(List<Customer> customerList)
+        {
+            //return customerList.OrderByDescending(c => c.LastName)
+            //                    .ThenByDescending(c => c.FirstName);
+
+            //Lub
+            return SortByName(customerList).Reverse();
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+
+namespace ACM.BL.Test
+{
+    
+    public class CustomerRepositoryTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [Test]
+        public void FindExistingCustomer()
+        {
+            // Arrange
+            CustomerRepository repository = new CustomerRepository();
+            var customerList = repository.Retrieve();
+
+            // Act
+            var result = repository.Find(customerList, 4);
+
+            // Assert
+           
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.CustomerId);
+            Assert.AreEqual("Solo", result.LastName);
+            Assert.AreEqual("Han", result.FirstName);
+        }
+        //Zadanie 1
+        // Napisz test jednostkowy sprawdzadzj¹cy dzia³anie naszej funckji "SortByName" zadeklarowanej w "CharactersRepository".
+        
+        
+        //Zadanie 2
+        // Napisz test jednostkowy sprawdzadzj¹cy dzia³anie naszej funckji "SortByNameInReverse" zadeklarowanej w "CharactersRepository".
+        
+
+    }
+}
